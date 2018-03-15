888470e15b9d89321aaf689e929f8aa13256a913
Maestroschan <rrroschan@gmail.com>
2018-03-15 19:06:57 +0100

    Avoid useless writing by using load_bytes

--- markdown_preview/__init__.py
+++ markdown_preview/__init__.py
@@ -14,6 +14,8 @@ try:
 except:
 	_ = lambda s: s
 	
+#################
+	
 MD_PREVIEW_KEY_BASE = 'org.gnome.gedit.plugins.markdown_preview'
 BASE_TEMP_NAME = '/tmp/gedit_plugin_markdown_preview'
 
@@ -33,10 +35,10 @@ class MarkdownGeditPluginApp(GObject.Object, Gedit.AppActivatable):
 	def _build_menu(self):
 		self.menu_ext = self.extend_menu('file-section-1')
 		menu = Gio.Menu()
-		sub_menu_item2 = Gio.MenuItem.new(_("Export the preview"), 'win.export_doc')
-		sub_menu_item3 = Gio.MenuItem.new(_("Print the preview"), 'win.print_doc')
-		menu.append_item(sub_menu_item2)
-		menu.append_item(sub_menu_item3)
+		menu_item_export = Gio.MenuItem.new(_("Export the preview"), 'win.export_doc')
+		menu_item_print = Gio.MenuItem.new(_("Print the preview"), 'win.print_doc')
+		menu.append_item(menu_item_export)
+		menu.append_item(menu_item_print)
 		self.menu_section = Gio.MenuItem.new_section(_("Markdown Preview"), menu)
 		self.menu_ext.append_menu_item(self.menu_section)
 	
@@ -58,47 +60,46 @@ class MarkdownGeditPluginWindow(GObject.Object, Gedit.WindowActivatable, PeasGtk
 	# This is called every time the gui is updated
 	def do_update_state(self):
 		if self.window.get_active_view() is not None:
-			if self.test_if_md():
-				self.panel.show()
 			if self._auto_reload:
 				self.on_reload(None, None)
+			if self.test_if_md():
+				self.panel.show()
+			elif len(self.panel.get_children()) is 1:
+				self.panel.hide()
 		
 	def do_activate(self):
 		# Defining the action which was set earlier in AppActivatable.
 		self._connect_menu()
 		self._settings = Gio.Settings.new(MD_PREVIEW_KEY_BASE)
 		self._isAtBottom = (self._settings.get_string('position') == 'bottom')
-		self._useRelativePaths = self._settings.get_boolean('relative')
 		self._settings.connect('changed::position', self.change_panel)
 		self.insert_in_adequate_panel()
 		self.window.connect('active-tab-changed', self.on_reload)
 		self.window.lookup_action('export_doc').set_enabled(False)
 		self.window.lookup_action('print_doc').set_enabled(False)
-		self.temp_file_html = Gio.File.new_for_path(BASE_TEMP_NAME + '.html')
 		self.temp_file_md = Gio.File.new_for_path(BASE_TEMP_NAME + '.md')
 
 	def _connect_menu(self):
-		action2 = Gio.SimpleAction(name='export_doc')
-		action3 = Gio.SimpleAction(name='print_doc')
-		action2.connect('activate', self.export_doc)
-		action3.connect('activate', self.print_doc)
-		self.window.add_action(action2)
-		self.window.add_action(action3)
+		action_export = Gio.SimpleAction(name='export_doc')
+		action_print = Gio.SimpleAction(name='print_doc')
+		action_export.connect('activate', self.export_doc)
+		action_print.connect('activate', self.print_doc)
+		self.window.add_action(action_export)
+		self.window.add_action(action_print)
 		
 	def insert_in_adequate_panel(self):
 		self._webview = WebKit2.WebView() # FIXME optimisable, ralentit tout le merdier
+		box = Gtk.Box()
 		
 		if self._isAtBottom:
 			self.preview_bar.props.orientation = Gtk.Orientation.HORIZONTAL
-		else:
-			self.preview_bar.props.orientation = Gtk.Orientation.VERTICAL
-		self.preview_bar.pack_start(self._webview, expand=True, fill=True, padding=0)
-		
-		box=Gtk.Box()
-		if self._isAtBottom:
 			box.props.orientation = Gtk.Orientation.VERTICAL
 		else:
+			self.preview_bar.props.orientation = Gtk.Orientation.VERTICAL
 			box.props.orientation = Gtk.Orientation.HORIZONTAL
+			
+		self.preview_bar.pack_start(self._webview, expand=True, fill=True, padding=0)
+		
 		box.props.margin_left = 5
 		box.props.margin_right = 5
 		box.props.margin_top = 5
@@ -147,8 +148,6 @@ class MarkdownGeditPluginWindow(GObject.Object, Gedit.WindowActivatable, PeasGtk
 			self._auto_reload = False
 	
 	def delete_temp_file(self):
-		if self.temp_file_html.query_exists():
-			self.temp_file_html.delete()
 		if self.temp_file_md.query_exists():
 			self.temp_file_md.delete()
 	
@@ -173,8 +172,6 @@ class MarkdownGeditPluginWindow(GObject.Object, Gedit.WindowActivatable, PeasGtk
 		if not self.test_if_md():
 			return
 		
-		dummy_uri = self.get_dummy_uri()
-
 		# Get the current document, or the temporary document if requested
 		doc = self.window.get_active_document()
 		if self._auto_reload:
@@ -183,34 +180,38 @@ class MarkdownGeditPluginWindow(GObject.Object, Gedit.WindowActivatable, PeasGtk
 			f = open(BASE_TEMP_NAME + '.md', 'w')
 			f.write(unsaved_text)
 			f.close()
-			file_uri = self.temp_file_md.get_uri()
+			file_path = self.temp_file_md.get_path()
 		else:
-			file_uri = doc.get_uri_for_display()
+			file_path = doc.get_location().get_path()
 		
 		# It uses pandoc to produce the html code
 		pre_string = '<html><head><meta charset="utf-8" /><link rel="stylesheet" href="' + \
 			self._settings.get_string('style') + '" /></head><body>'
 		post_string = '</body></html>'
-		result = subprocess.run(['pandoc', file_uri], stdout=subprocess.PIPE)
+		result = subprocess.run(['pandoc', file_path], stdout=subprocess.PIPE)
 		html_string = result.stdout.decode('utf-8')
 		html_content = pre_string + html_string + post_string
 		
-		if self._useRelativePaths:
-			self._webview.load_html(html_content, dummy_uri)
-		else:
-			f = open(BASE_TEMP_NAME + '.html', 'w')
-			f.write(html_content)
-			f.close()
-			self._webview.load_uri(self.temp_file_html.get_uri())
+		# The html code is converted into bytes
+		my_string = GLib.String()
+		my_string.append(html_content)
+		bytes_content = my_string.free_to_bytes()
+		
+		# This uri will be used as a reference for links and images using relative paths
+		dummy_uri = self.get_dummy_uri()
+		
+		# The content is loaded
+		self._webview.load_bytes(bytes_content, 'text/html', 'UTF-8', dummy_uri)
+		
 		self.window.lookup_action('export_doc').set_enabled(True)
 		self.window.lookup_action('print_doc').set_enabled(True)
 	
 	def get_dummy_uri(self):
 		# Support for relative paths is cool, but breaks CSS in many cases
-		if self._useRelativePaths:
-			return self.window.get_active_document().get_uri_for_display()
+		if self._settings.get_boolean('relative'):
+			return self.window.get_active_document().get_location().get_uri()
 		else:
-			return None
+			return 'file:///'
 	
 	def show_on_panel(self):
 		# Get the bottom bar (A Gtk.Stack), or the side bar, and add our bar to it.
@@ -238,13 +239,9 @@ class MarkdownGeditPluginWindow(GObject.Object, Gedit.WindowActivatable, PeasGtk
 			self._webview.set_zoom_level(self._webview.get_zoom_level() - 0.1)
 		
 	def on_insert(self, a):
-		doc = self.window.get_active_document()
 		
-		# It will not insert image if the document is not .md
-		name = doc.get_short_name_for_display()
-		temp = name.split('.')
-		if temp[len(temp)-1] != 'md':
-			self._webview.load_plain_text(_("This is not a markdown document."))
+		# Guard clause: it will not load dialog if the file is not .md
+		if not self.test_if_md():
 			return
 		
 		# Building a FileChooserDialog for pictures
@@ -260,6 +257,7 @@ class MarkdownGeditPluginWindow(GObject.Object, Gedit.WindowActivatable, PeasGtk
 		
 		# It gets the chosen file's path
 		if response == Gtk.ResponseType.OK:
+			doc = self.window.get_active_document()
 			picture_path = '![](' + file_chooser.get_filename() + ')'
 			iter = doc.get_iter_at_mark(doc.get_insert())
 			doc.insert(iter, picture_path)
@@ -291,6 +289,8 @@ class MarkdownGeditPluginWindow(GObject.Object, Gedit.WindowActivatable, PeasGtk
 	def print_doc(self, a, b):
 		p = WebKit2.PrintOperation.new(self._webview)
 		p.run_dialog()
+
+############################
 
 class MdConfigWidget:
 
@@ -366,8 +366,10 @@ class MdConfigWidget:
 		
 		# It gets the chosen file's path
 		if response == Gtk.ResponseType.OK:
-			self.styleLabel.label = file_chooser.get_uri()
-			self._settings.set_string('style', file_chooser.get_uri())
+#			self.styleLabel.label = file_chooser.get_uri() ##theorically better but harder to read for humans
+#			self._settings.set_string('style', file_chooser.get_uri())
+			self.styleLabel.label = file_chooser.get_filename()
+			self._settings.set_string('style', file_chooser.get_filename())
 		file_chooser.destroy()
 		
 	def on_relative_changed(self, w, a):
