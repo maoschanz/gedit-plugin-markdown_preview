834e9332de4042752c6dd580c709254517278194
Romain F. T <rrroschan@gmail.com>
2018-11-12 16:33:29 +0100

    Add most actions for issue #4
    Keyboard shortcuts don't work (why?) and the behavior regarding blocks can be improved (new line when beginning a list ? '```' tags if several lines of monospace ? etc.)

--- markdown_preview/__init__.py
+++ markdown_preview/__init__.py
@@ -55,11 +55,13 @@ class MarkdownGeditPluginApp(GObject.Object, Gedit.AppActivatable):
 
 	def add_accelerators(self):
 		self.app.add_accelerator("<Primary>E", "win.md-prev-insert-picture", None)
+		self.app.add_accelerator("<Primary>B", "win.md-prev-format-bold", None) # FIXME ?????
 #		self.app.add_accelerator("<Primary><Shift>M", "win.uncomment", None)
 		return
 
 	def remove_accelerators(self):
 		self.app.remove_accelerator("win.md-prev-insert-picture", None)
+		self.app.remove_accelerator("win.md-prev-format-bold", None)
 #		self.app.remove_accelerator("win.uncomment", None)
 		return
 
@@ -147,6 +149,22 @@ class MarkdownGeditPluginWindow(GObject.Object, Gedit.WindowActivatable, PeasGtk
 		
 		#-------------------------
 		
+		action_remove = Gio.SimpleAction(name='md-prev-remove-all')
+		action_remove.connect('activate', lambda i, j: self.view_method('remove_all'))
+		
+		action_title_1 = Gio.SimpleAction(name='md-prev-format-title-1')
+		action_title_1.connect('activate', lambda i, j: self.view_method('format_title_1'))
+		action_title_2 = Gio.SimpleAction(name='md-prev-format-title-2')
+		action_title_2.connect('activate', lambda i, j: self.view_method('format_title_2'))
+		action_title_3 = Gio.SimpleAction(name='md-prev-format-title-3')
+		action_title_3.connect('activate', lambda i, j: self.view_method('format_title_3'))
+		action_title_4 = Gio.SimpleAction(name='md-prev-format-title-4')
+		action_title_4.connect('activate', lambda i, j: self.view_method('format_title_4'))
+		action_title_5 = Gio.SimpleAction(name='md-prev-format-title-5')
+		action_title_5.connect('activate', lambda i, j: self.view_method('format_title_5'))
+		action_title_6 = Gio.SimpleAction(name='md-prev-format-title-6')
+		action_title_6.connect('activate', lambda i, j: self.view_method('format_title_6'))
+		
 		action_bold = Gio.SimpleAction(name='md-prev-format-bold')
 		action_bold.connect('activate', lambda i, j: self.view_method('format_bold'))
 		action_italic = Gio.SimpleAction(name='md-prev-format-italic')
@@ -157,18 +175,35 @@ class MarkdownGeditPluginWindow(GObject.Object, Gedit.WindowActivatable, PeasGtk
 		action_monospace.connect('activate', lambda i, j: self.view_method('format_monospace'))
 		action_quote = Gio.SimpleAction(name='md-prev-format-quote')
 		action_quote.connect('activate', lambda i, j: self.view_method('format_quote'))
-		# TODO
+		
+		action_unordered = Gio.SimpleAction(name='md-prev-list-unordered')
+		action_unordered.connect('activate', lambda i, j: self.view_method('list_unordered'))
+		action_ordered = Gio.SimpleAction(name='md-prev-list-ordered')
+		action_ordered.connect('activate', lambda i, j: self.view_method('list_ordered'))
+		
 		action_picture = Gio.SimpleAction(name='md-prev-insert-picture')
 		action_picture.connect('activate', lambda i, j: self.view_method('insert_picture'))
+		action_link = Gio.SimpleAction(name='md-prev-list-ordered')
+		action_link.connect('activate', lambda i, j: self.view_method('insert_link'))
 		action_table = Gio.SimpleAction(name='md-prev-insert-table')
 		action_table.connect('activate', lambda i, j: self.view_method('insert_table'))
 		
+		self.window.add_action(action_remove)
 		self.window.add_action(action_bold)
 		self.window.add_action(action_italic)
 		self.window.add_action(action_underline)
 		self.window.add_action(action_monospace)
-		# TODO
+		self.window.add_action(action_quote)
+		self.window.add_action(action_unordered)
+		self.window.add_action(action_ordered)
+		self.window.add_action(action_title_1)
+		self.window.add_action(action_title_2)
+		self.window.add_action(action_title_3)
+		self.window.add_action(action_title_4)
+		self.window.add_action(action_title_5)
+		self.window.add_action(action_title_6)
 		self.window.add_action(action_picture)
+		self.window.add_action(action_link)
 		self.window.add_action(action_table)
 	
 	def view_method(self, name):
@@ -187,6 +222,9 @@ class MarkdownGeditPluginWindow(GObject.Object, Gedit.WindowActivatable, PeasGtk
 			v.insert_table()
 		elif name == 'insert_picture':
 			v.insert_picture(self.window)
+		elif name == 'insert_link':
+			v.insert_link(self.window)
+			
 		elif name == 'format_bold':
 			v.format_bold()
 		elif name == 'format_italic':
@@ -197,8 +235,25 @@ class MarkdownGeditPluginWindow(GObject.Object, Gedit.WindowActivatable, PeasGtk
 			v.format_quote()
 		elif name == 'format_underline':
 			v.format_underline()
-		# TODO
-	
+			
+		elif name == 'list_ordered':
+			v.list_ordered()
+		elif name == 'list_unordered':
+			v.list_unordered()
+			
+		elif name == 'format_title_1':
+			v.format_title(1)
+		elif name == 'format_title_2':
+			v.format_title(2)
+		elif name == 'format_title_3':
+			v.format_title(3)
+		elif name == 'format_title_4':
+			v.format_title(4)
+		elif name == 'format_title_5':
+			v.format_title(5)
+		elif name == 'format_title_6':
+			v.format_title(6)
+
 	def on_file_changed(self, *args):
 		self.preview.file_format = self.recognize_format()
 		self.preview.on_reload()
@@ -216,7 +271,7 @@ class MarkdownGeditPluginWindow(GObject.Object, Gedit.WindowActivatable, PeasGtk
 		elif temp[len(temp)-1] == 'tex':
 			return 'tex'
 		if doc.is_untitled():
-			self.preview.display_warning(True, _("Can't preview an unsaved document"))
+			self.preview.display_warning(True, _("Can't preview an unsaved document")) # FIXME
 		else:
 			self.preview.display_warning(True, _("Unsupported type of document: ") + name)
 		return 'error'
@@ -238,7 +293,7 @@ class MarkdownGeditPluginWindow(GObject.Object, Gedit.WindowActivatable, PeasGtk
 	########
 	
 	def do_create_configure_widget(self):
-		# Just return your box, PeasGtk will automatically pack it into a dialog and show it.
+		# Just return a box, PeasGtk will automatically pack it into a dialog and show it.
 		widget = MdConfigWidget(self.plugin_info.get_data_dir())
 		return widget
 
@@ -290,7 +345,7 @@ class MarkdownGeditPluginView(GObject.Object, Gedit.ViewActivatable):
 			item.set_sensitive(False)
 		popup.append(item)
 	
-	def recognize_format(self):
+	def recognize_format(self): # FIXME doc.get_language()
 		doc = self.view.get_buffer()
 		name = doc.get_short_name_for_display()
 		temp = name.split('.')
@@ -304,19 +359,62 @@ class MarkdownGeditPluginView(GObject.Object, Gedit.ViewActivatable):
 		
 	################
 	
-	def format_bold(self):
+	def add_block_tags(self, start_tag, end_tag):
 		pass
+	
+	def add_line_tags(self, start_tag, end_tag):
+		document = self.view.get_buffer()
+		selection = document.get_selection_bounds()
+		if selection != ():
+			(start, end) = selection
+			if start.ends_line():
+				start.forward_line()
+			elif not start.starts_line():
+				start.set_line_offset(0)
+			if end.starts_line():
+				end.backward_char()
+			elif not end.ends_line():
+				end.forward_to_line_end()
+		else:
+			start = document.get_iter_at_mark(document.get_insert())
+			end = document.get_iter_at_mark(document.get_insert())
+		new_code = self.add_tags_characters(document, start_tag, end_tag, start, end)
+	
+	def add_word_tags(self, start_tag, end_tag):
+		document = self.view.get_buffer()
+		selection = document.get_selection_bounds()
+		if selection != ():
+			(start, end) = selection
+		else:
+			return
+		new_code = self.add_tags_characters(document, start_tag, end_tag, start, end)
+	
+	def format_title(self, level):
+		level = level + 1
+		self.add_line_tags('#'*level + ' ', ' ' + '#'*level)
+		
+	def format_bold(self):
+		self.add_word_tags('**', '**')
+		
+	def list_unordered(self):
+		self.add_line_tags('- ', '')
+		
+	def list_ordered(self):
+		self.add_line_tags('1. ', '')
 	
 	def format_italic(self):
-		pass
+		self.add_word_tags('*', '*')
 	
 	def format_monospace(self):
-		pass
+		self.add_word_tags('`', '`')
 	
 	def format_quote(self):
-		pass
+		self.add_line_tags('> ', '')
 		
 	def format_underline(self):
+		self.add_word_tags('__', '__')
+	
+	def insert_link(self, window):
 		pass
 	
 	def insert_picture(self, window):
@@ -344,5 +442,46 @@ class MarkdownGeditPluginView(GObject.Object, Gedit.ViewActivatable):
 		table = '|||\n|--|--|\n|||'
 		iter = doc.get_iter_at_mark(doc.get_insert())
 		doc.insert(iter, table)
+		
+	def add_tags_characters(self, document, start_tag, end_tag, start, end):
+		smark = document.create_mark("start", start, False)
+		imark = document.create_mark("iter", start, False)
+		emark = document.create_mark("end", end, False)
+		number_lines = end.get_line() - start.get_line() + 1
+		document.begin_user_action()
+
+		for i in range(0, number_lines):
+			iter = document.get_iter_at_mark(imark)
+			if not iter.ends_line():
+				document.insert(iter, start_tag)
+				if end_tag is not None:
+					if i != number_lines -1:
+						iter = document.get_iter_at_mark(imark)
+						iter.forward_to_line_end()
+						document.insert(iter, end_tag)
+					else:
+						iter = document.get_iter_at_mark(emark)
+						document.insert(iter, end_tag)
+			iter = document.get_iter_at_mark(imark)
+			iter.forward_line()
+			document.delete_mark(imark)
+			imark = document.create_mark("iter", iter, True)
+
+		document.end_user_action()
+
+		document.delete_mark(imark)
+		new_start = document.get_iter_at_mark(smark)
+		new_end = document.get_iter_at_mark(emark)
+		if not new_start.ends_line():
+			self.backward_tag(new_start, start_tag)
+		document.select_range(new_start, new_end)
+		document.delete_mark(smark)
+		document.delete_mark(emark)
+
+	def forward_tag(self, iter, tag):
+		iter.forward_chars(len(tag))
+
+	def backward_tag(self, iter, tag):
+		iter.backward_chars(len(tag))
 
 ##################################################
